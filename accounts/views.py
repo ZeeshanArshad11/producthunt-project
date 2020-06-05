e62532dd3e821bcf8cc1037d535beb2151295292
Zeeshan Arshad <zeeshanarshad11@gmail.com>
2020-06-05 17:57:20 +0500

    remove error in signup function

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,7 @@ def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2'] :
             try:
-                user = User.objects.POST(username=request.POST['username'])
+                user = User.objects.get(username=request.POST['username'])
                 return render(request, 'accounts/signup.html', {'error':'User already exist!'})
             except User.DoesNotExist:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
